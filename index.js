c7edf9adf3505454e67266b450819ae5566c844b
Vojtěch Kozák <kozak.v@gmail.com>
2016-08-09 09:10:25 +0200

    #4 First run doesn't create initial index

--- index.js
+++ index.js
@@ -23,52 +23,66 @@
     elasticIndexIncementer.createNewIndex = (client, alias, mapping, callback) => {
         async.autoInject({
             alias        : callback => callback(null, alias),
-            indexOld     : callback => elasticIndexIncementer.getRealElasticName(client, alias, callback),
-            indexNew     : (indexOld, callback) => callback(null, incrementGeoIndex(indexOld)),
+            indexOld     : callback => getIndexName(client, alias, callback),
+            indexNew     : (indexOld, callback) => callback(null, incrementIndex(indexOld)),
             uploadMapping: (indexNew, callback) => uploadMapping(client, indexNew, mapping, callback),
+            addAlias     : (indexNew, uploadMapping, callback) => createAliasForNewIndex(client, indexNew, alias, callback)
         }, (err, results) => callback(err, results));
     };
 
     elasticIndexIncementer.switchAlias = (client, indexInfo, callback) => {
-        async.series({
-            removeAlias: (callback, results) => updateAlias(client, 'remove', indexInfo.indexOld, indexInfo.alias, callback),
-            addAlias   : (callback, results) => updateAlias(client, 'add', indexInfo.indexNew, indexInfo.alias, callback),
-        }, (err, resp) => callback(err, resp));
+        if (getVersionFromIndexName(indexInfo.indexOld) !== 0) {
+            async.series({
+                removeAlias: (callback, results) => updateAlias(client, 'remove', indexInfo.indexOld, indexInfo.alias, callback),
+                addAlias   : (callback, results) => updateAlias(client, 'add', indexInfo.indexNew, indexInfo.alias, callback),
+            }, (err, resp) => callback(err, resp));
+        }
     };
 
     elasticIndexIncementer.removeOldIndex = (client, indexOld, callback) => {
-        logger.debug("removing old search index");
-        client.indices.delete({index: indexOld}, (err, resp) => callback(err, resp));
+        if (getVersionFromIndexName(indexOld) !== 0) {
+            logger.debug("removing old search index");
+            client.indices.delete({index: indexOld}, (err, resp) => callback(err, resp));
+        }
     };
 
-    elasticIndexIncementer.getRealElasticName = (client, alias, callback) => {
+    function createAliasForNewIndex(client, indexNew, alias, callback) {
+        return (getVersionFromIndexName(indexNew) === 1) ?
+            updateAlias(client, 'add', indexNew, alias, callback) :
+            callback();
+    }
+
+    function getIndexName(client, alias, callback) {
         getInfoAboutIndex(client, alias, (err, resp) => {
-            let realName = _.findKey(resp, 'aliases');
-            logger.debug(`alias ${alias} realname: ${realName}`);
-            callback(err, realName);
+            return (err && err.status === 404) ?
+                callback(null, `${alias}_v0`) :
+                callback(null, _.findKey(resp, 'aliases'));
         });
-    };
+    }
 
     function getInfoAboutIndex(client, alias, callback) {
         client.indices.get({index: alias}, (err, resp) => callback(err, resp));
     }
 
-    function incrementGeoIndex(geoIndexOld) {
-        if (!geoIndexOld) {
-            logger.info("new geo index name: geo_v1");
-            return "geo_v1";
-        }
+    function incrementIndex(indexOld) {
+        let version = getVersionFromIndexName(indexOld) + 1;
+        let newIndexName = `${getIndexNameWithoutVersion(indexOld)}_v${version}`;
 
-        let version = Number(geoIndexOld.substr(geoIndexOld.length - 1, geoIndexOld.length)) + 1;
-        let newGeoIndexName = geoIndexOld.slice(0, -1) + version;
-
-        logger.info(`new geo index name: ${newGeoIndexName}`);
-        return newGeoIndexName;
+        logger.info(`new index name: ${newIndexName}`);
+        return newIndexName;
     }
 
-    function uploadMapping(client, geoIndex, mapping, callback) {
-        logger.debug("setting up new geo index with appropriate mapping");
-        client.indices.create({index : geoIndex, body: mapping}, () => callback());
+    function getVersionFromIndexName(indexName) {
+        return Number(indexName.substr(indexName.length - 1, indexName.length));
+    }
+
+    function getIndexNameWithoutVersion(index) {
+        return index.slice(0, -3);
+    }
+
+    function uploadMapping(client, indexName, mapping, callback) {
+        logger.debug("setting up new index with appropriate mapping");
+        client.indices.create({index: indexName, body: mapping}, () => callback());
     }
 
     function updateAlias(client, action, index, alias, callback) {
